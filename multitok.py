f42639288461f8c9a09bbc03d3f4c3fd047f2721
financiallyruined <139030112+financiallyruined@users.noreply.github.com>
2024-04-28 15:09:20 +0000

    Added photo download

--- multitok.py
+++ multitok.py
@@ -22,9 +22,29 @@ parser.add_argument("--workers", default=3, help="Number of concurrent downloads
 
 args = parser.parse_args()
 
+def downloader(file_name, link, response, extension):
+    file_size = int(response.headers.get("content-length", 0))
+    folder_name = link.split("/")[-3]
+
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+        print(f"Folder created: {folder_name}\n")
+
+    with open(f"{folder_name}/{file_name}.{extension}", 'wb') as video_file, tqdm(
+        total=file_size,
+        unit='iB',
+        unit_scale=True,
+        unit_divisor=1024,
+        bar_format='{percentage:3.0f}%|{bar:20}{r_bar}{desc}', colour='green', desc=f"[{file_name}]"
+    ) as progress_bar:
+        for data in response.iter_content(chunk_size=1024):
+            size = video_file.write(data)
+            progress_bar.update(size)
+
 
 with open(args.links, "r") as links:
     tiktok_links = links.read().split("\n")
+
 
     def download(link):
         with requests.Session() as s:
@@ -33,43 +53,31 @@ with open(args.links, "r") as links:
 
                 soup = BeautifulSoup(response.content, 'html.parser')
                 token = soup.find("input", {"name": "token"})["value"]
-
                 data = {'url': link, 'token': token,}
 
-                response = s.post('https://tmate.cc/download', headers=headers, data=data)
+                response = s.post('https://tmate.cc/action', headers=headers, data=data).json()["data"]
 
-                soup = BeautifulSoup(response.content, 'html.parser')
+                soup = BeautifulSoup(response, 'html.parser')
 
-                if args.no_watermark:
-                    download_link = soup.find(class_="downtmate-right is-desktop-only right").find_all("a")[0]["href"]
-                elif args.watermark:
-                    download_link = soup.find(class_="downtmate-right is-desktop-only right").find_all("a")[3]["href"]
+                if not "photo" in link:
+                    download_link_index = 3 if args.watermark else 0
+                    file_name = link.split("/")[-1]
+                    download_link = soup.find(class_="downtmate-right is-desktop-only right").find_all("a")[download_link_index]["href"]
+                    response = s.get(download_link, stream=True, headers=headers)
+
+                    downloader(file_name, link, response, extension="mp4")
                 else:
-                    download_link = soup.find(class_="downtmate-right is-desktop-only right").find_all("a")[0]["href"]
+                    download_links = [i["src"] for i in soup.find_all(class_="card-img-top")]
+                    for index, download_link in enumerate(download_links):
+                        file_name = f"{link.split('/')[-1]}_{index}"
+                        response = s.get(download_link, stream=True, headers=headers)
 
-                response = requests.get(download_link, stream=True, headers=headers)
+                        downloader(file_name, link, response, extension="jpeg", )
 
-                file_size = int(response.headers.get("content-length", 0))
-                file_name = link.split("/")[-1]
-                folder_name = link.split("/")[-3]
-
-                if not os.path.exists(folder_name):
-                    os.mkdir(folder_name)
-                    print("Folder created:", folder_name)
-
-                with open(f"{folder_name}/{file_name}.mp4", 'wb') as video_file, tqdm(
-                    total=file_size,
-                    unit='iB',
-                    unit_scale=True,
-                    unit_divisor=1024,
-                    bar_format='{percentage:3.0f}%|{bar:20}{r_bar}{desc}', colour='green', desc=f"[{file_name}]"
-                ) as progress_bar:
-                    for data in response.iter_content(chunk_size=1024):
-                        size = video_file.write(data)
-                        progress_bar.update(size)
-            except:
-                with open("errors.txt", 'a') as error:
-                    error.write(link + "\n")
+            except Exception:
+                print(f"\033[91merror\033[0m: {link}")
+                with open("errors.txt", 'a') as error_file:
+                    error_file.write(link + "\n")
 
 if __name__ == "__main__":                
     with futures.ThreadPoolExecutor(max_workers=args.workers) as executor:
